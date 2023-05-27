330fa448b728d501895f713e830f61fb4d172684
Akash Padampalle <akashpadampalle780@gmail.com>
2023-05-27 12:32:33 +0530

    use map function to to iterate over array and covert each item to JSX

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,25 @@
 function ListGroup() {
+
+    const items = [
+        'New Delhi',
+        'New Mumbai',
+        'Kolkata',
+        'Surat',
+        'Tamil Nadu'
+    ];
+
+
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+        <h1>List Title</h1>
+        <ul className="list-group">
+          {items.map((item, index) => <li className="list-group-item" key={index}>{item}</li>)}
+        </ul>
+    </>
   );
 }
 
 export default ListGroup;
+
+
